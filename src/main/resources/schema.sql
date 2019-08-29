a7aa55931697584edd4dd67cc7ee5266c3b72bdc
fan <610398596@qq.com>
2019-08-29 19:52:36 +0800

    创建表

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,28 @@ CREATE TABLE employee (
   name VARCHAR(64) NOT NULL,
   age   int(4) NOT NULL
 );
+CREATE TABLE SystemAdministrator (
+  sysAdmID varchar(36) PRIMARY KEY,
+  username VARCHAR(64) NOT NULL,
+  password  VARCHAR(64) NOT NULL
+
+);
+CREATE TABLE Company (
+  CompanyID varchar(36) PRIMARY KEY,
+  CompanyAdminID VARCHAR(64) NOT NULL,
+  CompanyName  VARCHAR(64) NOT NULL,
+  SysAdmID varchar(36) NOT NULL ,
+    FOREIGN KEY (SysAdmID) REFERENCES SystemAdministrator ( sysAdmID ),
+    FOREIGN KEY (CompanyAdminID) REFERENCES CompanyAdminsitrator(CompanyAdminID )
+);
+CREATE TABLE Employee(
+  EmployeeID varchar(36) PRIMARY KEY,
+  CompanyID varchar(36) NOT NULL,
+  FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID )
+);
+CREATE TABLE CompanyAdminsitrator(
+  CompanyAdminID varchar(36) PRIMARY KEY,
+  CompanyAdminName varchar(36) NOT NULL,
+    EmployeeID varchar(36) NOT NULL,
+  FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID )
+);
